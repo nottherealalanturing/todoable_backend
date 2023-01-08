01b3f53b9332ef08543d0594b6c76407fa8ee4dd
Assad Isah <nottherealalanturing@gmail.com>
2023-01-08 23:36:39 +0100

    add rubocop linter to project

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,26 +1,27 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    respond_to :json
-    private
-    def respond_with(resource, _opts = {})
-        register_success && return if resource.persisted?
-        
-        register_failed
-    end
+  respond_to :json
 
-    def register_success
-        render json: {
-            message: 'Account Created.',
-            user: current_user
-        },
-        status: :created
-    end
+  private
 
+  def respond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
 
-    def register_failed
-        render json: {
-            message: 'Couldn\'t create account.',
-            errors: resource.errors
-        },
-        status: :unprocessable_entity
-    end
+    register_failed
+  end
+
+  def register_success
+    render json: {
+             message: 'Account Created.',
+             user: current_user
+           },
+           status: :created
+  end
+
+  def register_failed
+    render json: {
+             message: 'Couldn\'t create account.',
+             errors: resource.errors
+           },
+           status: :unprocessable_entity
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,33 +1,34 @@
 class Users::SessionsController < Devise::SessionsController
-    respond_to :json
-    private
-    def respond_with(resource, _opts = {})
-        render json: {
-            message: 'You\'re logged in.',
-            user: current_user
-        },
-        status: :ok
-    end
+  respond_to :json
 
-    def respond_to_on_destroy
-        log_out_success && return if current_user
-        
-        log_out_failure
-    end
+  private
 
+  def respond_with(_resource, _opts = {})
+    render json: {
+             message: 'You\'re logged in.',
+             user: current_user
+           },
+           status: :ok
+  end
 
-    def log_out_success
-        render json: {
-            message: 'You\'re logged out.',
-        },
-        status: :accepted
-    end
+  def respond_to_on_destroy
+    log_out_success && return if current_user
 
-    def log_out_failure
-        render json: {
-            message: 'Something went wrong, try again.',
-            errors: resource.errors
-        },
-        status: :expectation_failed
-    end
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: {
+             message: 'You\'re logged out.'
+           },
+           status: :accepted
+  end
+
+  def log_out_failure
+    render json: {
+             message: 'Something went wrong, try again.',
+             errors: resource.errors
+           },
+           status: :expectation_failed
+  end
 end
