de27cf87f26bf3556414d95947789c887eaf205d
Assad Isah <nottherealalanturing@gmail.com>
2023-03-12 09:50:36 +0100

    add web console whitelist

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module HealthcareCenter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.web_console.permissions = '102.91.4.160'
 
     # Configuration for the application, engines, and railties goes here.
     #
